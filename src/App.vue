8f4583d7a5df03f425b88d036e2570d4ff836c90
manupunkk <manuelejaradelahoz@gmail.com>
2026-08-19 17:44:17 -0400

    actividad 5 ahora si

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <Tareas />
+    <contacto />
   </div>
 </template>
 
 <script>
 import Tareas from './components/Tareas.vue';
+import Contacto from './components/Contacto.vue';
 export default {
-  components: { Tareas }
+  components: { Tareas, Contacto }
 }
 </script>
